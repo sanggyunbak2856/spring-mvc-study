cd05ef77739563339d3de0fc16437f69f6982a4d
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-08-28 19:01:00 +0900

    first

---
+++ src/main/java/hello/servlet/SpringMvcStudyApplication.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcStudyApplication.class, args);
+    }
+
+}
